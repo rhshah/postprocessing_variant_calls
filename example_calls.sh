36fb4eb0cf40f388b48a1290d2b45a393164f0c6
buehlere <buehlere77@gmail.com>
2022-04-28 14:23:56 -0400

    fixing up tests
    - fixing tests to work with new framework
    - small adjustment to example call

--- example_calls.sh
+++ example_calls.sh
@@ -1,4 +1,4 @@
 # Single Sample Call 
-pv vardict single filter --inputVcf data/Myeloid200-1.vcf  --tsampleName Myeloid200-1  -ad 1 -o data/single --single
+pv vardict single filter --inputVcf data/Myeloid200-1.vcf  --tsampleName Myeloid200-1  -ad 1 -o data/single 
 # Two Sample Call
 pv vardict case-control filter --inputVcf data/C-C1V52M-L001-d.DONOR22-TP.vardict.vcf  --tsampleName C-C1V52M-L001-d  -ad 1 -o data/two 
--- tests/vardict/test_vardict.py
+++ tests/vardict/test_vardict.py
@@ -3,35 +3,41 @@ import pytest  # type: ignore
 import os 
 from typer.testing import CliRunner
 
-from src.vardict.vardict_process import app
+from pv import app
 
 runner = CliRunner()
-sample = {
-    'single':['--inputVcf','data/Myeloid200-1.vcf','--tsampleName','Myeloid200-1', '-ad', '1', '-o', 'data/single', '--single'], 
-    'two':['--inputVcf','data/C-C1V52M-L001-d.DONOR22-TP.vardict.vcf','--tsampleName','C-C1V52M-L001-d', '-ad', '1', '-o', 'data/two', '--two']
-}
+vardict_single_calls = [
+        ['vardict', 'single', 'filter', '--inputVcf', 'data/Myeloid200-1.vcf' , '--tsampleName', 'Myeloid200-1', '-ad','1', '-o', 'data/single'], 
 
-def test_single():
-    result = runner.invoke(app, sample['single'])
+]
+
+vardict_matched = [
+    ['vardict', 'case-control', 'filter', '--inputVcf', 'data/C-C1V52M-L001-d.DONOR22-TP.vardict.vcf' , '--tsampleName', 'C-C1V52M-L001-d', '-ad','1' , '-o', 'data/two']
+]
+
+@pytest.mark.parametrize("call", vardict_single_calls)
+def test_single(call):
+    # vardict_tests['single']
+    result = runner.invoke(app, call)
     assert result.exit_code == 0
     assert '' in result.stdout
     assert os.path.exists("data/single/Myeloid200-1_complex_STDfilter.vcf") == True
     assert os.path.exists("data/single/Myeloid200-1_STDfilter.vcf") == True
     assert os.path.exists("data/single/Myeloid200-1_STDfilter.txt") == True
+    os.remove("data/single/Myeloid200-1_complex_STDfilter.vcf")
+    os.remove("data/single/Myeloid200-1_STDfilter.vcf")
+    os.remove("data/single/Myeloid200-1_STDfilter.txt")
 
-def test_two():
-    result = runner.invoke(app, sample['two'])
+@pytest.mark.parametrize("call", vardict_single_calls)
+def test_two(call):
+    result = runner.invoke(app, call)
     assert result.exit_code == 0
     assert '' in result.stdout
     assert os.path.exists("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_complex_STDfilter.vcf") == True
     assert os.path.exists("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.vcf") == True
     assert os.path.exists("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.txt") == True
+    os.remove("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_complex_STDfilter.vcf")
+    os.remove("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.vcf")
+    os.remove("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.txt")
 
-
-@pytest.mark.parametrize("name", sample)
-def test_arg(name: str):
-    # test single sample 
-    test_single() 
-    # test two sample
-    test_two()
 
