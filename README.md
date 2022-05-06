123b87a7a9a9529666f729fd7dfc9f5fd8f77541
buehlere <buehlere77@gmail.com>
2022-05-06 11:09:36 -0400

    doc and project structure edits
    - changing data location and updating paths
    - deleting un-used config files
    - updating README

--- README.md
+++ README.md
@@ -29,34 +29,8 @@ See `example_calls.sh` for more example calls.
 
 Template used: https://github.com/yxtay/python-project-template
 
-### Features
-
-- environment management with Conda
-- project metadata and dependency management with Poetry
-- preconfigured continuous integration tasks
-  - code formatting with isort and Black
-  - code linting with isort, Black, Flake8, Bandit and Mypy
-  - unit tests with pytest
-  - pre-commit hooks
-  - CICD pipelines with GitHub Actions
-- application
-  - logging with standard logging and python-json-logger
-  - configuration with standard configparser, python-dotenv and pydantic
-  - command line with Typer
-- deployment with Docker images
-  - development image based on `python:latest`
-  - lightweight production image based on `python:slim` using multi-stage build
-- Make formula for common development tasks
-  - install dependencies
-  - run continuous integration tasks
-  - run application
-  - build Docker images
-
 ### Usage
 
-Clone this repository or [use it as a template][generate] to generate a new repository.
-
-Update the project name and metadata in `pyproject.toml` and `configs/main.ini`.
 
 #### External dependencies
 
@@ -70,7 +44,7 @@ Use Conda to create a virtual environment and activate it for the project.
 
 ```bash
 conda env create -f environment.yml
-conda activate vardict
+conda activate pv_calls
 ```
 
 #### Install dependencies
@@ -90,47 +64,3 @@ conda env update -f environment.yml
 ```
 
 instead of `conda create`, and then re-run `make deps-install`
-
-### Tools
-
-- Environment management
-  - [Conda][conda]
-  - [Poetry][poetry]
-  - [Docker][docker]
-- Linting & Testing
-  - [isort][isort]
-  - [Black][black]
-  - [Flake8][flake8]
-  - [Bandit][bandit]
-  - [Mypy][mypy]
-  - [pytest][pytest]
-  - [pre-commit][pre-commit]
-- Application
-  - [logging][logging]
-  - [python-json-logger][python-json-logger]
-  - [configparser][configparser]
-  - [pydantic][pydantic]
-  - [Typer][typer]
-  - [Make][make]
-
-[conda]: https://docs.conda.io/en/latest
-[poetry]: https://python-poetry.org
-[isort]: https://timothycrosley.github.io/isort
-[black]: https://black.readthedocs.io/en/stable
-[flake8]: https://flake8.pycqa.org/en/latest
-[bandit]: https://github.com/PyCQA/bandit
-[mypy]: http://www.mypy-lang.org
-[pytest]: https://docs.pytest.org/en/stable
-[pre-commit]: https://pre-commit.com
-[logging]: https://docs.python.org/3/library/logging.html
-[python-json-logger]: https://github.com/madzak/python-json-logger
-[configparser]: https://docs.python.org/3/library/configparser.html
-[python-dotenv]: https://saurabh-kumar.com/python-dotenv
-[pydantic]: https://pydantic-docs.helpmanual.io
-[typer]: https://typer.tiangolo.com
-[fastapi]: https://fastapi.tiangolo.com
-[uvicorn]: https://www.uvicorn.org
-[gunicorn]: https://gunicorn.org
-[make]: https://www.gnu.org/software/make
-[docker]: https://www.docker.com
-[generate]: https://github.com/yxtay/python-project-template/generate
--- tests/vardict/test_vardict.py
+++ tests/vardict/test_vardict.py
@@ -2,17 +2,17 @@
 import pytest  # type: ignore
 import os 
 from typer.testing import CliRunner
-
+from pdb import set_trace as bp
 from pv import app
 
 runner = CliRunner()
 vardict_single_calls = [
-        ['vardict', 'single', 'filter', '--inputVcf', 'data/Myeloid200-1.vcf' , '--tsampleName', 'Myeloid200-1', '-ad','1', '-o', 'data/single'], 
+        ['vardict', 'single', 'filter', '--inputVcf', 'tests/vardict/data/single_test.vcf' , '--tsampleName', 'Myeloid200-1', '-ad','1', '-o', 'tests/vardict/data/single'], 
 
 ]
 
 vardict_matched = [
-    ['vardict', 'case-control', 'filter', '--inputVcf', 'data/C-C1V52M-L001-d.DONOR22-TP.vardict.vcf' , '--tsampleName', 'C-C1V52M-L001-d', '-ad','1' , '-o', 'data/two']
+    ['vardict', 'case-control', 'filter', '--inputVcf', 'tests/vardict/data/case_control_test.vcf' , '--tsampleName', 'C-C1V52M-L001-d', '-ad','1' , '-o', 'tests/vardict/data/two']
 ]
 
 @pytest.mark.parametrize("call", vardict_single_calls)
@@ -21,23 +21,23 @@ def test_single(call):
     result = runner.invoke(app, call)
     assert result.exit_code == 0
     assert '' in result.stdout
-    assert os.path.exists("data/single/Myeloid200-1_complex_STDfilter.vcf") == True
-    assert os.path.exists("data/single/Myeloid200-1_STDfilter.vcf") == True
-    assert os.path.exists("data/single/Myeloid200-1_STDfilter.txt") == True
-    os.remove("data/single/Myeloid200-1_complex_STDfilter.vcf")
-    os.remove("data/single/Myeloid200-1_STDfilter.vcf")
-    os.remove("data/single/Myeloid200-1_STDfilter.txt")
+    assert os.path.exists("tests/vardict/data/single/single_test_complex_STDfilter.vcf") == True
+    assert os.path.exists("tests/vardict/data/single/single_test_STDfilter.vcf") == True
+    assert os.path.exists("tests/vardict/data/single/single_test_STDfilter.txt") == True
+    os.remove("tests/vardict/data/single/single_test_complex_STDfilter.vcf")
+    os.remove("tests/vardict/data/single/single_test_STDfilter.vcf")
+    os.remove("tests/vardict/data/single/single_test_STDfilter.txt")
 
 @pytest.mark.parametrize("call", vardict_matched)
 def test_two(call):
     result = runner.invoke(app, call)
     assert result.exit_code == 0
     assert '' in result.stdout
-    assert os.path.exists("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_complex_STDfilter.vcf") == True
-    assert os.path.exists("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.vcf") == True
-    assert os.path.exists("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.txt") == True
-    os.remove("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_complex_STDfilter.vcf")
-    os.remove("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.vcf")
-    os.remove("data/two/C-C1V52M-L001-d.DONOR22-TP.vardict_STDfilter.txt")
+    assert os.path.exists("tests/vardict/data/two/case_control_test_complex_STDfilter.vcf") == True
+    assert os.path.exists("tests/vardict/data/two/case_control_test_STDfilter.vcf") == True
+    assert os.path.exists("tests/vardict/data/two/case_control_test_STDfilter.txt") == True
+    os.remove("tests/vardict/data/two/case_control_test_complex_STDfilter.vcf")
+    os.remove("tests/vardict/data/two/case_control_test_STDfilter.vcf")
+    os.remove("tests/vardict/data/two/case_control_test_STDfilter.txt")
 
 
