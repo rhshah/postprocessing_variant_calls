1547a62719a23a445fdddc6b8fd16f2a0f4d09dc
Karthigayini Sivaprakasam <karthigayinigithub@gmail.com>
2023-01-24 14:29:04 -0600

    overlap

--- postprocessing_variant_calls/annotate/annotate_helpers.py
+++ postprocessing_variant_calls/annotate/annotate_helpers.py
@@ -16,6 +16,13 @@ def maf_bed_annotate(maf, bed):
     bed_df = pd.read_csv(bed, sep="\t", skiprows=skip, low_memory=False)
     print("bed")
     maf_df=maf_df.sort_values(by=['Chromosome', 'Start_Position', 'End_Position'])
+    maf_overlap=maf_df
+    maf_overlap["covered"]=bed_df.lookup_df(maf_df, "Chromosome", "Start_Position")
+    maf_overlap.loc[maf_overlap["covered"].notnull(), "covered"] = "yes"
+    maf_overlap.loc[maf_overlap["covered"].notna(), "covered"] = "yes"
+    maf_overlap.loc[maf_overlap["covered"].isnull(), "covered"] = "no"
+    maf_overlap.loc[maf_overlap["covered"].isna(), "covered"] = "no"
+    maf_overlap.drop_duplicates().to_csv(maf_df, sep="\t", index=False)
 
 def get_row(file):
     skipped = []
