3bb8f250422cc8b97627d207f7e1e58559a8cc16
carmelinacharalambous <charalk@mskcc.org>
2023-02-23 14:11:33 -0500

    adding remaining fields

--- postprocessing_variant_calls/maf/concat_helpers.py
+++ postprocessing_variant_calls/maf/concat_helpers.py
@@ -10,20 +10,20 @@ import typer
 import pandas as pd
 
 def check_maf(files: List[Path]):
-    # return non if argument is empty 
-    if files is None: 
-        return None 
+    # return non if argument is empty
+    if files is None:
+        return None
     # check that we have a list of mafs after reading off the cli
     extensions = [os.path.splitext(f)[1] for f in files]
     for ext in extensions:
         if ext != '.maf':
             typer.secho(f"If using files argument, all files must be mafs.", fg=typer.colors.RED)
             raise typer.Abort()
-    return files 
+    return files
 
 def check_txt(paths: Path):
     # return None if argument is empty, kind of unfortunate we have to handle this case
-    if paths is None: 
+    if paths is None:
         return None
     # check that we have a text file after reading off the cli
     extension = os.path.splitext(paths)[1]
@@ -34,13 +34,13 @@ def check_txt(paths: Path):
 
 def concat_mafs(files, output_maf):
     #TODO appending to empty data frame is slow, we sould make list of frames and flatten
-    final_df = pd.DataFrame() 
+    final_df = pd.DataFrame()
     for maf in files:
         if Path(maf).is_file():
             # Read maf file
             typer.secho(f"Reading: {maf}", fg=typer.colors.BRIGHT_GREEN)
             maf_df = pd.read_csv(maf, sep="\t", low_memory=True)
-            #TODO this should be some kind of imported structure or global 
+            #TODO this should be some kind of imported structure or global
             maf_col_df = maf_df[
                 [
                     "Hugo_Symbol",
@@ -52,6 +52,12 @@ def concat_mafs(files, output_maf):
                     "Variant_Classification",
                     "Variant_Type",
                     "Tumor_Sample_Barcode",
+                    "Matched_Norm_Sample_Barcode",
+                    "HGVSp_Short",
+                    "t_ref_count",
+                    "t_alt_count",
+                    "n_ref_count",
+                    "n_alt_count",
                 ]
             ]
             final_df = final_df.append(maf_col_df, ignore_index=True)
@@ -63,6 +69,6 @@ def concat_mafs(files, output_maf):
         f"Done processing the concatenation of maf files writing output to {output_maf} in maf format",
         fg=typer.colors.GREEN,
     )
-    # write final df and return 
+    # write final df and return
     final_df.to_csv(f"{output_maf}.maf", index=False, sep="\t")
     return 0
