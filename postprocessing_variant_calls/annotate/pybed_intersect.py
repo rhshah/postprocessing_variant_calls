3bcb1103c19974056bf1443e69fc77626385d823
buehlere <buehlere@mskcc.org>
2023-02-24 12:14:02 -0500

    Update pybed_intersect.py
    :heavy_check_mark: adding basic procedures, need to fix merge, and take advantage of sort

--- postprocessing_variant_calls/annotate/pybed_intersect.py
+++ postprocessing_variant_calls/annotate/pybed_intersect.py
@@ -14,11 +14,28 @@ def maf_bed_annotate(maf,bed,cname,outputFile):
     mafdf_sub=maf_df[['Chromosome', 'Start_Position', 'End_Position']]
     mafdf_dic=dict(mafdf_sub.set_index('Chromosome').groupby(level=0).apply(lambda x : x.to_dict(orient='records')))
     beddf_dic=dict(bed_df.set_index('Chromosome').groupby(level=0).apply(lambda x : x.to_dict(orient='records')))
+    maf_annotated = []
     for k1,v1 in mafdf_dic.items():
-        breakpoint()
-        #beddf_sub={key: value for key, value in beddf_dic.items() if value==v1}
-        #onion=dict((k, beddf_dic[k]) for k in k1)
-        print(k1,v1)
+        # subset to bed
+        subset_bed = beddf_dic[k1]
+        # iterate over locations in maf
+        for idx, loc_maf in enumerate(v1):
+            for loc_bed in subset_bed:
+                #TODO take advantage of sorted bed and maf
+                # logic for whether to include comment 
+                include_logic = loc_maf['Start_Position'] > loc_bed['Start_Position']  & loc_maf['Start_Position'] <= loc_bed['End_Position'] & loc_maf['End_Position'] <= loc_bed['End_Position'] 
+                if include_logic:
+                    # add comment to bed 
+                    loc_maf[cname] = loc_bed['Comment']
+                if not include_logic: 
+                    loc_maf[cname] = None 
+                mafdf_dic[k1][idx] = loc_maf
+        chr_df_maf = pd.DataFrame(mafdf_dic[k1])
+        chr_df_maf['Chromosome'] = k1
+        maf_annotated.append(chr_df_maf)
+    combined_annotations = pd.concat(maf_annotated, axis=0, ignore_index=True)
+    final_annotate = combined_annotations.merge(maf_df, on=['Start_Position','End_Position', 'Chromosome'], how='right')
+    final_annotate.to_csv(outputFile)
 
 def get_row(file):
     skipped = []
@@ -26,5 +43,5 @@ def get_row(file):
         skipped.extend(i for i, line in enumerate(csv_file) if line.startswith("#"))
     return skipped
 
-maf_bed_annotate("../in/C-C1V52M-L001-d.DONOR22-TP.vardict.maf", "../rmsk_mod.bed",
+maf_bed_annotate("/Users/ebuehler/Downloads/mount/C-C1V52M-L001-d.DONOR22-TP.vardict.maf", "/Users/ebuehler/Downloads/mount/rmsk_mod.bed",
 "complexity", "output.csv")
