2f2fa9865d357e0dd49b292120977c1314ba38ff
Rashmi Naidu <rnaidugit@gmail.com>
2024-01-29 13:28:23 -0500

    saving work thus far on mutect filter

--- postprocessing_variant_calls/main.py
+++ postprocessing_variant_calls/main.py
@@ -1,6 +1,7 @@
 # Main Import
 import typer
 from .vardict import vardict_process
+from .mutect import mutect_process
 # from .maf import main
 from .maf import main
 from .maf.annotate import annotate_process
@@ -13,6 +14,9 @@ app = typer.Typer()
 
 # Vardict filter App 
 app.add_typer(vardict_process.app, name="vardict", help="post-processing commands for VarDict version 1.4.6 VCFs.")
+
+# muTect filter App 
+app.add_typer(mutect_process.app, name="mutect", help="post-processing commands for MuTect version 1.1.5 VCFs.")
 
 # Add Annote Maf 
 app.add_typer(main.app, name="maf", help="operations for manipulating maf files based on a given input.")
