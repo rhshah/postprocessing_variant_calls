d6a81772d2ac8b0ada56ce64744a9f57c8949911
Rashmi Naidu <rnaidugit@gmail.com>
2024-08-08 13:28:55 -0400

    resolving maf tests for github actions check

--- tests/test_maf.py
+++ tests/test_maf.py
@@ -70,7 +70,6 @@ maf_tag = [
     ["maf", "tag", "truncating_mut_in_TSG", "--help"],
     ["maf", "tag", "traceback", "--help"],
     ["maf", "tag", "by_variant_classification", "--help"],
-    ["maf", "tag", "access", "--help"],
 ]
 
 
